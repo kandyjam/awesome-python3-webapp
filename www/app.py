026c980071a6aa2b17f1d5a9ea975895a38a3e7d
kandyjam <kandyjam@gmail.com>
2019-07-25 17:29:46 +0800

    format code

--- www/app.py
+++ www/app.py
@@ -1,22 +1,26 @@
-import logging; logging.basicConfig(level=logging.INFO)
+import logging;
 
-import asyncio, os, json, time
-from datetime import datetime
+logging.basicConfig(level=logging.INFO)
+
+import asyncio
 
 from aiohttp import web
 
+
 def index(request):
     print("index page")
-    return web.Response(text='<h1>Awesome</h1>',content_type='text/html')
+    return web.Response(text='<h1>Awesome</h1>', content_type='text/html')
+
 
 async def init(event_loop):
     app = web.Application()
-    app.router.add_route('GET','/',index)
+    app.router.add_route('GET', '/', index)
     runner = web.AppRunner(app)
     await runner.setup()
-    srv = web.TCPSite(runner,'127.0.0.1',9000)
+    srv = web.TCPSite(runner, '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000 ...')
     await srv.start()
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))

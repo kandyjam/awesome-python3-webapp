770a3d2fd372bd8b343a2c01f2d58cd150b06cd0
张井城 <kandyjam@zhangjingchengdeMacBook-Pro.local>
2019-07-25 17:28:03 +0800

    Initial commit

---
+++ www/app.py
@@ -0,0 +1,23 @@
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    print("index page")
+    return web.Response(text='<h1>Awesome</h1>',content_type='text/html')
+
+async def init(event_loop):
+    app = web.Application()
+    app.router.add_route('GET','/',index)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    srv = web.TCPSite(runner,'127.0.0.1',9000)
+    logging.info('server started at http://127.0.0.1:9000 ...')
+    await srv.start()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
